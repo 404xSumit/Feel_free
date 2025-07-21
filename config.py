9927518a85faf33e18ef51ce1e241cd530f9173c
Sumit Singh Thakur <sumit942538@gmail.com>
2025-07-21 10:45:56 +0530

    added database online

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sql12791046:NgAqldRUYy@sql12.freesqldatabase.com/sql12791046"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'feel_free_secret_2025'
